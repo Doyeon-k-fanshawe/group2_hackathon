eabe0ad98925f4df1292d63dc700fcb792e5f6a8
Doyeon <d_kim120803@fanshaweonline.ca>
2022-10-17 21:26:51 -0400

    adding Files and building basic HTML and CSS

---
+++ js/modules/dataMiner.js
@@ -0,0 +1,17 @@
+//creat a function to export
+
+function getData(targetURL, callback) {
+
+  fetch(targetURL) 
+
+  .then(res => res.json()) 
+
+  .then(data => {
+      console.log(data);
+      callback(data);
+  })
+
+  .catch(error => console.log(error));
+}
+
+export { getData }
